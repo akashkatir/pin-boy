a87ce6082d152f813fdc619b86f0456469695c1e
Tim Shur <tim.shur@gmail.com>
2024-08-23 20:56:01 -0400

    Update flip animation, input keys.

--- flipper.gd
+++ flipper.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("right_bumper"):
+	if Input.is_action_pressed("flip"):
+		$AnimationPlayer.stop()
 		$AnimationPlayer.play("flip")
-	#else:
-		#$AnimationPlayer.play("FlipDown")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
