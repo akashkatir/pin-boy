60ef50414d9de61e6d6086f23498b57a6808b532
Tim Shur <tim.shur@gmail.com>
2024-08-23 21:09:41 -0400

    Fix errors and warnings; add debounce.

--- flipper.gd
+++ flipper.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
+# Time before a flip action can be pressed again and have an effect.
+var debounce_time = 0.4;
+
 func _physics_process(_delta):
 	if Input.is_action_pressed("flip"):
-		$AnimationPlayer.stop()
+		if ($AnimationPlayer.is_playing() and
+			$AnimationPlayer.current_animation_position > debounce_time):
+			$AnimationPlayer.stop()
 		$AnimationPlayer.play("flip")
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +16,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
