d922170737a6af153c4c44b6fe122508937137e1
Tim Shur <tim.shur@gmail.com>
2024-09-01 12:25:35 -0400

    Change animation strategy for flippers.

--- Map/flipper.gd
+++ Map/flipper.gd
@@ -1,28 +1,29 @@
 extends Node3D
 
-@export var invert = false;
+@export var right_flipper = false;
 # Time before a flip action can be pressed again and have an effect.
 @export var debounce_time = 0.4;
 
-var animation_name: String = "flip";
+var action_name: String = "left_flip"
+var animation_name: String = "left_flip";
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("left_flip") and !invert:
+	if Input.is_action_just_pressed(action_name):
 		play_animation()
-	elif Input.is_action_just_pressed("right_flip") and invert:
-		play_animation()
+	if Input.is_action_just_released(action_name):
+		reset_animation()
 		
 func play_animation() -> void:
-	if ($AnimationPlayer.is_playing() and
-		$AnimationPlayer.current_animation_position > debounce_time):
-		$AnimationPlayer.stop()
-		
 	$AnimationPlayer.play(animation_name)
+	
+func reset_animation() -> void:
+	$AnimationPlayer.play("RESET")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if invert:
-		animation_name = "flip_inverted"
+	if right_flipper:
+		action_name = "right_flip"
+		animation_name = "right_flip"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
--- Map/piston.gd
+++ Map/piston.gd
@@ -11,7 +11,7 @@ func _process(_delta: float) -> void:
 	pass
 	
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("eject"):
+	if Input.is_action_just_pressed("eject"):
 		$AnimationPlayer.play("fire")
-	elif Input.is_action_just_released("right_flip"):
-		$AnimationPlayer.stop()
+	elif Input.is_action_just_released("eject"):
+		$AnimationPlayer.play("RESET")
