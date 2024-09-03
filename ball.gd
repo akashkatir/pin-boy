8e6c57f4f4c152bba77c3b073e95403d70f68888
akashkatir <130875454+akashkatir@users.noreply.github.com>
2024-09-03 12:25:23 -0700

    Add max_velocity

--- ball.gd
+++ ball.gd
@@ -11,6 +11,13 @@ func _process(_delta: float) -> void:
 	pass
 
 
+func _physics_process(delta: float) -> void:
+	#set a max velocity
+	linear_velocity.x = clamp(linear_velocity.x, -30, 30)
+	linear_velocity.z = clamp(linear_velocity.z, -30, 30)
+
+
+
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("bumper"):
 		print(body)
