5e43215a1063a79f01fd328df39694ebb56aa03f
Tim Shur <tim.shur@gmail.com>
2024-09-05 21:26:16 -0400

    Add title screen menu.

--- Interface/user_interface.gd
+++ Interface/user_interface.gd
@@ -3,8 +3,19 @@ extends Control
 
 var best_height: int = 0;  # Highest point reached in meters.
 
+
+func show_game() -> void:
+	$HUD.show()
+	$MainMenu.hide()
+	
+
+func show_menu() -> void:
+	$HUD.hide()
+	$MainMenu.show()
+
+
 func update_height(height: float):
 	height = int(height)  # Truncate from float.
 	best_height = max(best_height, height)
-	$CurrentHeightLabel.text = "Current height: %sm" % height
-	$BestHeightLabel.text = "Best height: %sm" % best_height
+	$HUD/CurrentHeightLabel.text = "Current height: %sm" % height
+	$HUD/BestHeightLabel.text = "Best height: %sm" % best_height
--- main.gd
+++ main.gd
@@ -1,15 +1,33 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
+@export var is_playing = false
+
+
 func _ready() -> void:
-	pass  # Replace with function body.
+	$UserInterface.show_menu()
 
 func _physics_process(_delta: float) -> void:
+	handle_menu()
+
 	if out_of_bounds($Ball.position):
-		$Ball.position = $BallSpawn.position
-		$Ball.linear_velocity = Vector3.ZERO
+		spawn_ball()
 	$UserInterface.update_height(-$Ball.position.z)
 	
+func handle_menu() -> void:
+	if is_playing:
+		if Input.is_action_just_released("menu"):
+			is_playing = false
+			$UserInterface.show_menu()
+	else:
+		if Input.is_anything_pressed():
+			spawn_ball()
+			$UserInterface.show_game()
+			is_playing = true
+			
+func spawn_ball() -> void:
+	$Ball.position = $BallSpawn.position
+	$Ball.linear_velocity = Vector3.ZERO
+
 func out_of_bounds(pos: Vector3) -> bool:
 	return pos.y < -1 or pos.z > 1
