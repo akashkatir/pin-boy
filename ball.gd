2ba2743b74e75ccc5466f39066a86c07f105b57a
Tim Shur <tim.shur@gmail.com>
2024-09-10 19:36:03 -0400

    Move bumpers to bumpermap and use physics material instead of code.

--- ball.gd
+++ ball.gd
@@ -3,12 +3,10 @@ extends RigidBody3D
 
 func _physics_process(_delta: float) -> void:
 	# Set a max velocity.
-	linear_velocity.x = clamp(linear_velocity.x, -30, 30)
-	linear_velocity.z = clamp(linear_velocity.z, -30, 30)
+	linear_velocity.x = clamp(linear_velocity.x, -50, 50)
+	linear_velocity.z = clamp(linear_velocity.z, -50, 50)
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("bumper"):
-		apply_central_impulse(linear_velocity * 1.5)
 	if body.is_in_group("boost"):
 		print("Touched boost")
 		var velocity_multiplier = Vector3(3,0,0)
