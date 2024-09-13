026eeb23953b2607ce0b758100b2a6ecb54b1c1d
Tim Shur <tim.shur@gmail.com>
2024-09-13 13:10:46 -0400

    Update camera tracking speed and position.

--- camera_pivot.gd
+++ camera_pivot.gd
@@ -11,5 +11,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var ball_position = ball.global_transform.origin
 	
-	#global_transform.origin.z = ball_position.z + 7
-	global_transform.origin.z = lerp(global_transform.origin.z, ball_position.z + 7, 5 * delta)
+	global_transform.origin.z = lerp(
+		global_transform.origin.z, ball_position.z + 5, delta)
