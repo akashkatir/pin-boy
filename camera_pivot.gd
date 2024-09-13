070362b215f61e8599c6531e096160bf41bed38b
akashkatir <130875454+akashkatir@users.noreply.github.com>
2024-09-12 18:23:01 -0700

    Camera moves with ball

---
+++ camera_pivot.gd
@@ -0,0 +1,15 @@
+extends Marker3D
+
+#@onready var ball = get_node("/root/Ball");
+@export var ball: Node3D;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var ball_position = ball.global_transform.origin
+	
+	#global_transform.origin.z = ball_position.z + 7
+	global_transform.origin.z = lerp(global_transform.origin.z, ball_position.z + 7, 5 * delta)
