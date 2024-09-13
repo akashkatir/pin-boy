08ce43f1701471892020279a2bee5c05014ed273
Tim Shur <tim.shur@gmail.com>
2024-09-13 18:45:34 -0400

    Add transparent boost back; change implementation.

---
+++ Map/boost.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+
+@export var velocity_multiplier = 3
+
+
+func _on_area_3d_body_entered(body: RigidBody3D) -> void:
+	if body.is_in_group("ball"):
+		print("Boost!")
+		body.linear_velocity *= velocity_multiplier
--- ball.gd
+++ ball.gd
@@ -5,9 +5,3 @@ func _physics_process(_delta: float) -> void:
 	# Set a max velocity.
 	linear_velocity.x = clamp(linear_velocity.x, -50, 50)
 	linear_velocity.z = clamp(linear_velocity.z, -50, 50)
-
-func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("boost"):
-		print("Touched boost")
-		var velocity_multiplier = Vector3(3,0,0)
-		linear_velocity *= velocity_multiplier
