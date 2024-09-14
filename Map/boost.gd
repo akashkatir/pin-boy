1527e2c002f8595b03d9693684401816ed9bba99
Tim Shur <tim.shur@gmail.com>
2024-09-14 14:28:09 -0400

    Tweak map.

--- Map/boost.gd
+++ Map/boost.gd
@@ -4,7 +4,7 @@ extends Node3D
 @export var velocity_multiplier = 3
 
 
-func _on_area_3d_body_entered(body: RigidBody3D) -> void:
+func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("ball"):
 		print("Boost!")
 		body.linear_velocity *= velocity_multiplier
