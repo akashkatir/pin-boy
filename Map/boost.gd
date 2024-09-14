30faa516c9a9bd77cf7f620029c80c341bcbc4e1
Tim Shur <tim.shur@gmail.com>
2024-09-14 14:45:36 -0400

    Add directional boosts.

--- Map/boost.gd
+++ Map/boost.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
-
-@export var velocity_multiplier = 3
+@export var velocity_multiplier = 5
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("ball"):
 		print("Boost!")
-		body.linear_velocity *= velocity_multiplier
+		var direction = -global_basis.z
+		body.linear_velocity += direction * velocity_multiplier
