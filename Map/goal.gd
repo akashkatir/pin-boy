49e339fd3c41a74c46b3a7fc0099bc2e9105dde8
Tim Shur <tim.shur@gmail.com>
2024-09-19 21:07:28 -0400

    Add goal that prints You Win after 2 seconds.

---
+++ Map/goal.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+@export var win_countdown_seconds: float = 2.0
+
+var countdown: float = win_countdown_seconds
+var is_in_goal: bool = false
+
+func _physics_process(delta: float) -> void:
+	if is_in_goal:
+		countdown -= delta
+		if countdown < 0:
+			win()
+
+func win() -> void:
+	print('You win!')
+	reset_timer()
+	
+func start_timer() -> void:
+	countdown = win_countdown_seconds
+	is_in_goal = true
+
+func reset_timer() -> void:
+	is_in_goal = false
+	countdown = win_countdown_seconds
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("ball"):
+		start_timer()
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("ball"):
+		reset_timer()
